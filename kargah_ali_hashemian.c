8eb3a422e3aae762267c4ac5a32b851221e2026c
hashemian1382 <aho.hashemian@gmail.com>
2022-12-02 22:25:10 +0330

    added features

--- kargah_ali_hashemian.c
+++ kargah_ali_hashemian.c
@@ -3,7 +3,8 @@
 int main(){
     int number;
     scanf("%d",&number);
-    printf("I can tell you 2 amazing!\n");
+    printf("I can tell you 3 amazing!\n");
     printf("tavan2=%d\n",number*number);
-    printf("radikalf2=%.3f\n",pow((double)number,0.5));  
+    printf("radikalf2=%.3f\n",pow((double)number,0.5)); 
+    printf("tavan3=%d\n",number*number);
 }

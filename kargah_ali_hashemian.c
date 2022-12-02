8645c2d4a91d65ea8906cfd589084066d7577650
hashemian1382 <aho.hashemian@gmail.com>
2022-12-02 22:18:05 +0330

    First Commite

---
+++ kargah_ali_hashemian.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int number;
+    scanf("%d",&number);
+    printf("I can tell you 2 amazing!\n");
+    printf("tavan2=%d\n",number*number);
+    printf("radikalf2=%.3f\n",pow((double)number,0.5));  
+}
